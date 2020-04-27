0da530dc91c579e9eaf8346b5dae046dab1e599d
Sarith Fernando <sarithf@gmail.com>
2020-04-27 15:56:55 +0100

    checking feature

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          I was changed
+          I was changed on the feature branch
         </p>
         <a
           className="App-link"
